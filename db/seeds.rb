d5b76615510a64cc5e522d90306d98a81f0d0cf7
hirohiro-Naito <76986626+hirohiro-Naito@users.noreply.github.com>
2021-01-06 01:31:30 +0000

    hogehoge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+# coding: utf-8
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+CreateShops.create(:name=>'東京',:imageurl=>'abc')
+CreateShops.create(:name=>'大阪',:imageurl=>'def')
+
+CreateUsers.create(:name=>'麻生太郎',:password=>'asou920',:point=>'5000')
+CreateUsers.create(:name=>'野田佳彦',:password=>'noda520',:point=>'1000')
+CreateUsers.create(:name=>'菅義偉',:password=>'suga126',:point=>'100')
