647238e705cc53ab2d826563f4f7cd15352bdc90
asamuzak09 <ekazumasa.09300201@gmail.com>
2021-01-06 01:30:10 +0000

    fix migrate

--- db/migrate/20210105065026_create_qrs.rb
+++ db/migrate/20210105065026_create_qrs.rb
@@ -2,7 +2,7 @@ class CreateQrs < ActiveRecord::Migration[5.2]
   def change
     create_table :qrs do |t|
       t.integer :type, null: false
-      t.integer :point, null: false
+      t.integer :point
       t.timestamps
     end
   end
