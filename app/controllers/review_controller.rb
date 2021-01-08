0423e7c32cdf658ef7741c38f21d1fb7788adde9
asamuzak09 <ekazumasa.09300201@gmail.com>
2021-01-08 05:59:12 +0000

    fix

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -7,7 +7,9 @@ class ReviewController < ApplicationController
     def create
         review = Review.new(review_params)
         if review.save
-            render json: { status: 'SUCCESS', data: review} 
+            user = User.find(params[:user_id])
+            user.update!(point: user.point += 10)  
+            render json: { status: 'SUCCESS', data: review, add_point: 10} 
         else
             render json: { status: 'ERROR', reviews: Review.find_by_product(review_params[:product_id]), data: review.errors }
         end
