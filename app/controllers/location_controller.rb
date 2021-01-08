bdfebd6eb0ab8c14bc099869de922c508e16bfc2
asamuzak09 <ekazumasa.09300201@gmail.com>
2021-01-08 01:25:01 +0000

    fix

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -1,7 +1,7 @@
 class LocationController < ApplicationController
     def getposition
         location = Location.find_location(params)
-        render json: { status: "SUCCESS", data: { x: location.x, y: location.y, map_image: location.shop.imageurl } }
+        render json: { status: "SUCCESS", data: { map_url: location.map_url, map_image: location.shop.imageurl } }
     end
 
 end    
